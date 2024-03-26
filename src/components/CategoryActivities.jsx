24176636c5dfb92672ad865e02452cec9ee4be46
Pawel <pawel.werbowy@gmail.com>
2024-03-26 10:45:56 +0000

    styling added vto activities list

--- src/components/CategoryActivities.jsx
+++ src/components/CategoryActivities.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "../firebaseConfig";
-
+import "../App.css";
 const CategoryActivities = ({ selectedCategory }) => {
   const [activities, setActivities] = useState([]);
 
@@ -31,13 +31,16 @@ const CategoryActivities = ({ selectedCategory }) => {
   }, [selectedCategory]);
 
   return (
-    <div>
+    <div className="activities-list">
       <h2>Activities in "{selectedCategory}"</h2>
+      {/* i want to have a bar that shows date activity and time and i will be able to sort my recorded activities by date time and description. Also i want the activities to be displayed by date by default */}
       {activities.length > 0 ? (
         <ul>
           {activities.map((activity) => (
-            <li key={activity.id}>
-              {activity.date} - {activity.subject} - {activity.hours}
+            <li key={activity.id} className="activity-item">
+              <span className="activity-date">{activity.date}</span>
+              <span className="activity-subject">{activity.subject}</span>
+              <span className="activity-hours">{activity.hours} hrs</span>
             </li>
           ))}
         </ul>
